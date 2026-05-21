1d6c38d8e05a1f630aca136b01cdaddee83e39c1
Phil de Joux <philderbeast@gmail.com>
2026-05-21 17:17:43 -0400

    open Units.DMS in LatLng

--- latlng/LatLng.ml
+++ latlng/LatLng.ml
@@ -1,8 +1,8 @@
-type t = { lat : Units.DMS.rad; lng : Units.DMS.rad }
+open Units.DMS
+
+type t = { lat : rad; lng : rad }
 
 let to_string (x : t) : string =
-  let lat_dms = Units.DMS.from_deg (Units.DMS.from_rad x.lat) in
-  let lng_dms = Units.DMS.from_deg (Units.DMS.from_rad x.lng) in
-  Printf.sprintf "(%s, %s)"
-    (Units.DMS.display_dms lat_dms)
-    (Units.DMS.display_dms lng_dms)
+  let lat_dms = from_deg (from_rad x.lat) in
+  let lng_dms = from_deg (from_rad x.lng) in
+  Printf.sprintf "(%s, %s)" (display_dms lat_dms) (display_dms lng_dms)
